a674fb5760271ce27abcf9b06a3eeea0ae3b7a7e
acaditya10 <acadityachandra@gmail.com>
2026-07-22 13:58:16 +0530

    fix: dynamic import Footer in blogs layout to match homepage

--- app/blogs/layout.tsx
+++ app/blogs/layout.tsx
@@ -1,7 +1,9 @@
-﻿import { AdminProvider } from "@/components/AdminProvider";
+﻿import dynamic from "next/dynamic";
+import { AdminProvider } from "@/components/AdminProvider";
 import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer";
 import FloatingCTA from "@/components/FloatingCTA";
+
+const Footer = dynamic(() => import("@/components/Footer"), { ssr: false });
 
 export default function BlogsLayout({ children }: { children: React.ReactNode }) {
   return (
