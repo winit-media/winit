2914fa4d26c36eef8c3c44448208c839c15ef934
acaditya10 <acadityachandra@gmail.com>
2026-07-22 13:59:24 +0530

    fix: remove ssr:false from blogs layout Footer import

--- app/blogs/layout.tsx
+++ app/blogs/layout.tsx
@@ -3,7 +3,7 @@ import { AdminProvider } from "@/components/AdminProvider";
 import Navbar from "@/components/Navbar";
 import FloatingCTA from "@/components/FloatingCTA";
 
-const Footer = dynamic(() => import("@/components/Footer"), { ssr: false });
+const Footer = dynamic(() => import("@/components/Footer"));
 
 export default function BlogsLayout({ children }: { children: React.ReactNode }) {
   return (
